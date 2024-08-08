603762b7bb8a1a54890f649c166a1c28cbe81ff7
duckymoomoo <enyewphua@gmail.com>
2024-08-09 00:39:33 +0800

    Update Danny's Diner SQL Case Study.sql

--- 1. Danny's Diner 8wkSQLchallenge/Danny's Diner SQL Case Study.sql
+++ 1. Danny's Diner 8wkSQLchallenge/Danny's Diner SQL Case Study.sql
@@ -1,3 +1,4 @@
+-- Link to Case Study https://8weeksqlchallenge.com/case-study-1/
 -- 1. What is the total amount each customer spent at the restaurant?
 -- 2. How many days has each customer visited the restaurant?
 -- 3. What was the first item from the menu purchased by each customer?
